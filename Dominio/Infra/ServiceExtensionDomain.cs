d4443b7384b984afeb1504b84c7e052ef35fd6a1
Eduardo Cizeski Meneghel <eduardocmeneghel@gmail.com>
2024-09-06 20:40:50 -0300

    Configurando dominio e aplicação

---
+++ Dominio/Infra/ServiceExtensionDomain.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public static class ServiceExtensionDomain
+    {
+        public static void ConfigurarDominio(this IServiceCollection services,
+                                                 IConfiguration configuration)
+        {
+            services.ConfigurarRepositorio(configuration);
+        }
+    }
+}
