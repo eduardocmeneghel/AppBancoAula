014630212d0f012e2d62cbce8a1aaf06bde14603
Eduardo Cizeski Meneghel <eduardocmeneghel@gmail.com>
2024-09-06 20:14:21 -0300

    Configurando repositório

---
+++ Repositorio/Infra/GeradorDeServicos.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio
+{
+    public class GeradorDeServicos
+    {
+        public static ServiceProvider serviceProvider;
+
+        public static DataContext CarregarContexto()
+        {
+            return serviceProvider.GetService<DataContext>();
+        }
+
+    }
+}
---
+++ Repositorio/Infra/ServicesExtesionRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio
+{
+    public static class ServicesExtesionRepository
+    {
+        public static void ConfigurarRepositorio(this IServiceCollection services,
+                                                 IConfiguration configuration)
+        {
+            var enderecoBanco = configuration.GetConnectionString("Sqlite");
+            services.AddDbContext<DataContext>(opt => opt.UseSqlite(enderecoBanco));
+
+            GeradorDeServicos.serviceProvider = services.BuildServiceProvider();
+        }
+    }
+}
